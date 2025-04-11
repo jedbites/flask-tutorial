bb02fca3f071fb7dcba454528205f3ffa50b8b61
jedbites <canon.jayde03@gmail.com>
2025-04-11 17:27:50 +0800

    Added blog related functions and templates.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,7 +10,28 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register', methods=['GET', 'POST'])
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone())
+
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -29,6 +50,7 @@ def register():
                     "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
                 )
+                db.commit()
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
@@ -36,7 +58,7 @@ def register():
         flash(error)
     return render_template("auth/register.html")
 
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -54,35 +76,14 @@ def login():
 
         if error is None:
             session.clear()
-            session['user-id'] = user['id']
+            session['user_id'] = user['id']
             return redirect(url_for('index'))
         
         flash(error)
 
     return render_template('auth/login.html')
 
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE username =?', (user_id,)
-        ).fetchone()
-
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-        
-        return view(**kwargs)
-    
-    return wrapped_view
